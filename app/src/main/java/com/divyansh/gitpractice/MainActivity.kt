342832bfe6ac52ab4211d2a93449d9794bf8a79b
be-the-nerd <divyanshverma010@gmail.com>
2021-11-20 20:17:08 +0530

    Change 3 commit

--- app/src/main/java/com/divyansh/gitpractice/MainActivity.kt
+++ app/src/main/java/com/divyansh/gitpractice/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         println("This is contained in my second commit")
         println("Experimental")
         println("Change 2")
+        println("This line will be merged")
     }
 }
